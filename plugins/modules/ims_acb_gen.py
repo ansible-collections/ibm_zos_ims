4d7d94f58c6a6b9a3fc3941474e138ee9c28129b
BRYANT PANYARACHUN <bpanyarachun@gmail.com>
2020-05-21 21:00:47 -0700

    update header.

--- plugins/modules/ims_acb_gen.py
+++ plugins/modules/ims_acb_gen.py
@@ -1,6 +1,6 @@
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
-
-# Copyright (c) IBM Corporation 2019, 2020
+# Copyright (c) IBM Corporation 2020
 # Apache License, Version 2.0 (see https://opensource.org/licenses/Apache-2.0)
 
 from __future__ import absolute_import, print_function
@@ -13,6 +13,7 @@ ANSIBLE_METADATA = {
     "supported_by": "community",
 }
 DOCUMENTATION = r'''
+---
 module: ims_acb_gen
 short_description: Generate ACBs for PSB and DBD
 description:
