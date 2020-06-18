7f94132bf969c1355e1c013f65ed6978f0bd3789
Jerry Li <lij@us.ibm.com>
2020-06-18 18:09:30 -0400

    change control statements to a dictionary

--- plugins/modules/ims_catalog_populate.py
+++ plugins/modules/ims_catalog_populate.py
@@ -421,132 +421,138 @@ options:
       - Defines the output messagea dataset
     type: str
     required: false
-  duplist:
+  control_statements:
     description:
-      - Specifies that the DFS3PU00 utility list each DBD or PSB resource in the input ACB 
-        library that is not added to the IMS catalog because it is a duplicate of an instance 
-        that is already in the IMS catalog. 
-        # Lots of if cases in this one regarding managed acb, do I need to list all of them?
-    type: bool
-    required: false
-  errormax:
-    description:
-      - Terminate the IMS Catalog Populate utility when more than n messages indicate errors that prevent 
-        certain DBDs and PSBs from having their metadata that is written to the IMS catalog. 
-    type: int
-    required: false
-  resource_chkp_freq:
-    description:
-      - Specifies the number of DBD and PSB resource instances to be inserted between checkpoints. 
-        n can be a 1 to 8 digit numeric value of 1 to 99999999. 
-    type: int
-    required: false
-  segment_chkp_freq:
-    description:
-      - Specifies the number of segments to be inserted between checkpoints. Can be a 1 to 8 digit numeric value of 1 to 99999999.
-    type: int
-    required: false
-  isrtlist:
-    description:
-      - If the IMS management of ACBs is enabled, the utility also lists each DBD or PSB resources that is either added 
-        to the IMS directory or saved to the staging data set for importing into the IMS directory later.
-    type: bool
-    required: false
-  managed_acbs:
-    description:
-      - Use the MANAGEDACBS control statement to perform the following actions:
-        Set up IMS to manage the runtime application control blocks for your databases and program views.
-        Update an IMS system that manages ACBs with new or modified ACBs from an ACB library data set.
-        Save ACBs from an ACB library to a staging data set for later importing into an IMS system that manages ACBs.
+      - The control statement parameters
     type: dict
     required: false
     suboptions:
-      setup:
+      duplist:
         description:
-          - Creates the IMS directory datasets that are required by IMS to manage application control blocks
+          - Specifies that the DFS3PU00 utility list each DBD or PSB resource in the input ACB 
+            library that is not added to the IMS catalog because it is a duplicate of an instance 
+            that is already in the IMS catalog. 
+            # Lots of if cases in this one regarding managed acb, do I need to list all of them?
         type: bool
         required: false
-      stage:
-        description: 
-          - Saves ACBs from the input ACB libraries to a staging dataset.
-        type: dict
-        required: false
-        suboptions:
-          latest:
-            description:
-              - If an ACB already exists in the IMS system, do not save an instance of the ACB from an ACB library unless
-                the instance in the ACB library has a later timestamp than the ACB in the IMS system.
-            type: bool
-            required: false
-          uncond:
-            description:
-              - If an ACB already exists in the IMS system, save an instance of the ACB from an ACB library unconditionally, 
-                unless the timestamp of the ACB in the ACB library is the same as the timestamp of the ACB in the IMS system. 
-            type: bool
-            required: false
-          delete:
-            description:
-              - If the staging data set is not allocated to any online IMS system, scratch and recreate the staging data 
-                set before adding the resources to the staging data set.
-            type: bool
-            required: false
-          gsampcb:
-            description:
-              - GSAM resources are included for MANAGEDACBS= running in DLI mode using PSB DFSCP001. When GSAMPCB is specified, 
-                the IEFRDER batch log data set is not used by the catalog members information gather task.
-            type: bool
-            required: false
-          gsamdbd:
-            description:
-              - The name of the changed GSAM database. You can use the gsamdbd variable with the STAGE or UPDATE parameter. 
-                However, LATEST, UNCOND, DELETE, SHARE, and GSAMPCB are not supported if you specify the gsamdbd variable.          
-            type: str
-            required: false
-      update:
+      errormax:
         description:
-          - Updates existing IMS directory system datasets directly in exclusive mode. The ACBs are not placed in the 
-            staging dataset. 
+          - Terminate the IMS Catalog Populate utility when more than n messages indicate errors that prevent 
+            certain DBDs and PSBs from having their metadata that is written to the IMS catalog. 
+        type: int
+        required: false
+      resource_chkp_freq:
+        description:
+          - Specifies the number of DBD and PSB resource instances to be inserted between checkpoints. 
+            n can be a 1 to 8 digit numeric value of 1 to 99999999. 
+        type: int
+        required: false
+      segment_chkp_freq:
+        description:
+          - Specifies the number of segments to be inserted between checkpoints. Can be a 1 to 8 digit numeric value of 1 to 99999999.
+        type: int
+        required: false
+      isrtlist:
+        description:
+          - If the IMS management of ACBs is enabled, the utility also lists each DBD or PSB resources that is either added 
+            to the IMS directory or saved to the staging data set for importing into the IMS directory later.
+        type: bool
+        required: false
+      managed_acbs:
+        description:
+          - Use the MANAGEDACBS control statement to perform the following actions:
+            Set up IMS to manage the runtime application control blocks for your databases and program views.
+            Update an IMS system that manages ACBs with new or modified ACBs from an ACB library data set.
+            Save ACBs from an ACB library to a staging data set for later importing into an IMS system that manages ACBs.
         type: dict
         required: false
         suboptions:
-          latest:
+          setup:
             description:
-              - If an ACB already exists in the IMS system, do not replace it with an instance of the ACB from an ACB library unless
-                the instance in the ACB library has a later timestamp than the ACB in the IMS system.
+              - Creates the IMS directory datasets that are required by IMS to manage application control blocks
             type: bool
             required: false
-          uncond:
-            description:
-              - If an ACB already exists in the IMS system, replace it with an instance of the ACB from an ACB library unconditionally, 
-                unless the timestamp of the ACB in the ACB library is the same as the timestamp of the ACB in the IMS system. 
-            type: bool
+          stage:
+            description: 
+              - Saves ACBs from the input ACB libraries to a staging dataset.
+            type: dict
             required: false
-          share:
+            suboptions:
+              latest:
+                description:
+                  - If an ACB already exists in the IMS system, do not save an instance of the ACB from an ACB library unless
+                    the instance in the ACB library has a later timestamp than the ACB in the IMS system.
+                type: bool
+                required: false
+              uncond:
+                description:
+                  - If an ACB already exists in the IMS system, save an instance of the ACB from an ACB library unconditionally, 
+                    unless the timestamp of the ACB in the ACB library is the same as the timestamp of the ACB in the IMS system. 
+                type: bool
+                required: false
+              delete:
+                description:
+                  - If the staging data set is not allocated to any online IMS system, scratch and recreate the staging data 
+                    set before adding the resources to the staging data set.
+                type: bool
+                required: false
+              gsampcb:
+                description:
+                  - GSAM resources are included for MANAGEDACBS= running in DLI mode using PSB DFSCP001. When GSAMPCB is specified, 
+                    the IEFRDER batch log data set is not used by the catalog members information gather task.
+                type: bool
+                required: false
+              gsamdbd:
+                description:
+                  - The name of the changed GSAM database. You can use the gsamdbd variable with the STAGE or UPDATE parameter. 
+                    However, LATEST, UNCOND, DELETE, SHARE, and GSAMPCB are not supported if you specify the gsamdbd variable.          
+                type: str
+                required: false
+          update:
             description:
-              - For dynamic option (DOPT) PSBs only, allocates the required IMS directory data sets in a shared mode 
-                so that DOPT PSBs can be added to the IMS catalog without interrupting online processing.
-            type: bool
+              - Updates existing IMS directory system datasets directly in exclusive mode. The ACBs are not placed in the 
+                staging dataset. 
+            type: dict
             required: false
-          gsampcb:
-            description:
-              - GSAM resources are included for MANAGEDACBS= running in DLI mode using PSB DFSCP001. When GSAMPCB is specified, 
-                the IEFRDER batch log data set is not used by the catalog members information gather task.
-            type: bool
-            required: false
-          gsamdbd:
-            description:
-              - The name of the changed GSAM database. You can use the gsamdbd variable with the STAGE or UPDATE parameter. 
-                However, LATEST, UNCOND, DELETE, SHARE, and GSAMPCB are not supported if you specify the gsamdbd variable.          
-            type: str
-            required: false
-  no_isrtlist:
-    description:
-      - Do not print a list of inserted resource instances.
-    type: bool
-    required: false
-    
-       
-     
+            suboptions:
+              latest:
+                description:
+                  - If an ACB already exists in the IMS system, do not replace it with an instance of the ACB from an ACB library unless
+                    the instance in the ACB library has a later timestamp than the ACB in the IMS system.
+                type: bool
+                required: false
+              uncond:
+                description:
+                  - If an ACB already exists in the IMS system, replace it with an instance of the ACB from an ACB library unconditionally, 
+                    unless the timestamp of the ACB in the ACB library is the same as the timestamp of the ACB in the IMS system. 
+                type: bool
+                required: false
+              share:
+                description:
+                  - For dynamic option (DOPT) PSBs only, allocates the required IMS directory data sets in a shared mode 
+                    so that DOPT PSBs can be added to the IMS catalog without interrupting online processing.
+                type: bool
+                required: false
+              gsampcb:
+                description:
+                  - GSAM resources are included for MANAGEDACBS= running in DLI mode using PSB DFSCP001. When GSAMPCB is specified, 
+                    the IEFRDER batch log data set is not used by the catalog members information gather task.
+                type: bool
+                required: false
+              gsamdbd:
+                description:
+                  - The name of the changed GSAM database. You can use the gsamdbd variable with the STAGE or UPDATE parameter. 
+                    However, LATEST, UNCOND, DELETE, SHARE, and GSAMPCB are not supported if you specify the gsamdbd variable.          
+                type: str
+                required: false
+      no_isrtlist:
+        description:
+          - Do not print a list of inserted resource instances.
+        type: bool
+        required: false
+
+
+
 
 author:
   - Jerry Li 
@@ -608,13 +614,7 @@ def run_module():
       directory_staging_dataset=dict(type="str", required=False),
       proclib=dict(type="str", required=False),
       steplib=dict(type="str", required=False),
-      duplist=dict(type="bool", required=False),
-      errormax=dict(type="int", required=False),
-      resource_chkp_freq=dict(type="int", required=False),
-      segment_chkp_freq=dict(type="int", required=False),
-      isrtlist=dict(type="bool", required=False),
-      managed_acbs=dict(type="dict", required=False),
-      no_isrtlist=dict(type="bool", required=False)
+      control_statements=dict(type="dict", required=False)
     )
 
     global module
