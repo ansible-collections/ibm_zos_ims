8b95d3530aa64fdc4a6aab5ee67a0cb0548aed25
Omar Elbarmawi <omar@ibm.com>
2020-06-29 18:25:12 -0700

    documentation and slight modifications

--- plugins/module_utils/IMSDbrc.py
+++ plugins/module_utils/IMSDbrc.py
@@ -30,6 +30,25 @@ class IMSDbrc():
     }
 
     def __init__(self, commands, steplib, dynalloc=None, dbdlib=None, genjcl=None, recon1=None, recon2=None, recon3=None):
+        """DBRC object used to create and run DBRC commands using zos_raw.
+
+        Args:
+            commands (str, list[str]): List of the DBRC commands to be executed.
+            steplib (str, list[str]): List of STEPLIB datasets that contain the IMS nucleus and the
+                required action modules.
+            dynalloc (str, optional): The DYNALLOC data set that will be used to complete the DBRC
+                execution. Required if 'recon1', 'recon2', and 'recon3' are not specified. Defaults to None.
+            dbdlib (str, optional): The data set that contains the database descriptions for the databases
+                that are under the control of DBRC. Defaults to None.
+            genjcl (str, optional): The PDS, which contains the JCL and control statements for the utility
+                that DBRC uses to generate a job. Defaults to None.
+            recon1 (str, optional): The RECON1 data set that will be used to complete the DBRC execution.
+                Defaults to None. Required if 'dynalloc' is not specified.
+            recon2 (str, optional): The RECON2 data set that will be used to complete the DBRC execution.
+                Defaults to None. Required if 'dynalloc' is not specified.
+            recon3 (str, optional): The RECON3 data set that will be used to complete the DBRC execution.
+                Defaults to None. Required if 'dynalloc' is not specified.
+        """
         self.commands = commands
         self.steplib_list = steplib
         self.dynalloc = dynalloc
@@ -41,15 +60,30 @@ class IMSDbrc():
         self._assert_valid_inputs()
 
     def _assert_valid_inputs(self):
+        """Validates the data types and requirements to run DBRC commands.
+        """
         self._assert_valid_input_types()
         self._assert_dynalloc_recon_requirement()
 
     def _assert_dynalloc_recon_requirement(self):
+        """This assertion function validates that either the 'dynalloc' parameter was specified, or 
+        all of the recon parameters were specified. This is a requirement to run the DBRC utility.
+
+        Raises:
+            ValueError: Neither dynalloc or any recon data sets were specified.
+        """
         # TODO: Determine if each of the RECONs need to be present or just 1 minimum
         if not self.dynalloc and not (self.recon1 or self.recon2 or self.recon3):
             raise ValueError("'dynalloc' or ('recon1', 'recon2', 'recon3') must be specified.")
 
     def _assert_valid_input_types(self):
+        """This assertion function validates that all parameters are the correct data type. All
+        parameters in the constructor must be strings except for 'commands' and 'steplib' which
+        can also be a list of strings.
+
+        Raises:
+            TypeError: Raised if parameter is the wrong data type.
+        """
         if isinstance(self.commands, str):
             self.commands = [self.commands]
         elif not isinstance(self.commands, list) and any(not isinstance(cmd, str) for cmd in self.commands):
@@ -74,10 +108,23 @@ class IMSDbrc():
         
         if not isinstance(self.recon3, str):
             raise TypeError("'recon3' must be a string.")
-        
     
-    def _extract_values(self, elements):
+    def _extract_values(self, output_line):
+        """Given a line from the output string, this function parses a line that contains
+        equals signs (=), and returns a dictionary with key value pairs based on the line.
+
+        Args:
+            output_line (str): The original line of output given from the DBRC utility result.
+
+        Returns:
+            (dict): Mapping of the fields that corresponds to the line from the output
+        
+        Example:
+            output_line: 'FORCER    LOG DSN CHECK=CHECK44    STARTNEW=NO'
+            returns: {"LOG DSN CHECK": "CHECK44", "STARTNEW": False}
+        """
         fields = {}
+        elements = output_line.split("=")
         i = 0
         while i < len(elements) - 1:
             key_list = list(filter(None, elements[i].split("  ")))
@@ -105,14 +152,39 @@ class IMSDbrc():
         return fields
 
     def _format_command(self, raw_command):
+        """Properly formats the specific command corresponding to the output being parsed.
+
+        Args:
+            raw_command (str): Unformatted str that contains the current command.
+
+        Returns:
+            (str): Formatted command string.
+        """
         if raw_command[0] == "0":
             return raw_command[1:].strip()
         return raw_command.strip()
 
     def _get_indentifier(self, raw_output_line):
+        """Returns the "identifier" of the particular block of output being parsed.
+
+        Args:
+            raw_output_line (str): Unformatted line from the output that contains the identifier.
+
+        Returns:
+            (str): Formatted identifier extracted from the provided line of output.
+        """
         return raw_output_line.split("  ")[0].strip()
 
     def _parse_output(self, raw_output):
+        """Main function that parses the entire output provided by the zos_raw module.
+
+        Args:
+            raw_output (str): Unformatted output text provided from zos_raw module.
+
+        Returns:
+            (dict): Parsed output mappings.
+            (list[str]): Original output provided by zos_raw.
+        """
         original_output = [elem.strip() for elem in raw_output.split("\n")]
         output_fields = {}
         command = ''
@@ -135,31 +207,56 @@ class IMSDbrc():
                 output_id = {"IDENTIFIER": self._get_indentifier(original_output[index + 1])}
                 output_fields[command]['OUTPUT'][output_index].update(output_id)
             elif "=" in line:
-                elements = line.split("=")
-                output_fields[command]['OUTPUT'][output_index].update(self._extract_values(elements))
+                output_fields[command]['OUTPUT'][output_index].update(self._extract_values(line))
             elif re.search(dsp_pattern, line, re.IGNORECASE):
                 output_fields[command]['MESSAGES'].append(line)
 
         return output_fields, original_output
 
-    def _build_command(self):
+    def _add_utility_statement(self, name, data_set, dbrc_utility_fields):
+        """Adds the DD Statement to the list of dbrc_utility_fields if the data set name
+        was provided.
+
+        Args:
+            name (str): Name parameter to provide to DDStatement constructor.
+            data_set (str): Name of the dataset to be provided to the DatasetDefinition cosntructor.
+            dbrc_utility_fields (list[str]): List that contains DDStatement objects.
+        """
+        if data_set:
+            dbrc_utility_fields.append(DDStatement(name, DatasetDefinition(data_set)))
+
+    def _build_utility_statements(self):
+        """Builds the list DDStatements that will be provided to the zos_raw execute function
+        based on the user input.
+
+        Returns:
+            (list[DDStatement]): List of DDStatements
+        """
         dbrc_utility_fields = []
-        steplib_datasets = [DatasetDefinition(steplib) for steplib in self.steplib_list]
-        steplib = DDStatement("steplib", steplib_datasets)
-        recon1 = DDStatement("recon1", DatasetDefinition(self.recon1))
-        recon2 = DDStatement("recon2", DatasetDefinition(self.recon2))
-        recon3 = DDStatement("recon3", DatasetDefinition(self.recon3))
-        jclpds = DDStatement("jclpds", DatasetDefinition(self.genjcl))
-        ims = DDStatement("ims", DatasetDefinition(self.dbdlib))
-        dbrc_commands = [StdinDefinition("\n".join(self.commands))]
+        steplib_data_set_definitions = [DatasetDefinition(steplib) for steplib in self.steplib_list]
+        if self.dynalloc:
+            steplib_data_set_definitions.append(DatasetDefinition(self.dynalloc))
+        steplib = DDStatement("steplib", steplib_data_set_definitions)
+        dbrc_utility_fields.append(steplib)
+        self._add_utility_statement("recon1", self.recon1, dbrc_utility_fields)
+        self._add_utility_statement("recon2", self.recon2, dbrc_utility_fields)
+        self._add_utility_statement("recon3", self.recon3, dbrc_utility_fields)
+        self._add_utility_statement("jclpds", self.genjcl, dbrc_utility_fields)
+        self._add_utility_statement("ims", self.dbdlib, dbrc_utility_fields)
+        dbrc_commands = StdinDefinition("\n".join(self.commands))
         sysin = DDStatement("sysin", dbrc_commands)
         sysprint = DDStatement("sysprint", StdoutDefinition())
-
-        dbrc_utility_fields = [steplib, recon1, recon2, recon3, jclpds, ims, sysin, sysprint]
+        dbrc_utility_fields.extend([sysin, sysprint])
         return dbrc_utility_fields
 
     def execute(self):
-        dbrc_utility_fields = self._build_command()
+        """Executes the DBRC utility dspurx00 using the zos_raw module based on the user input.
+
+        Returns:
+            (dict): (1) The original, unformatted output provided by zos_raw and (2) the parsed
+                mappings for the output.
+        """
+        dbrc_utility_fields = self._build_utility_statements()
         response = MVSCmd.execute("dspurx00", dbrc_utility_fields)
         fields, original_output = self._parse_output(response.stdout)
         # fields, original_output = self._parse_output(TEST_INPUT)
