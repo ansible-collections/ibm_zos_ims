7676aa2a14bf3df32a924f02dc4eb442e8587081
Jerry Li <lij@us.ibm.com>
2020-07-21 18:04:08 -0400

    fix dangling merge conflicts, get simple catalog load test working

--- tests/functional/module_utils/ims_test_catalog_utils.py
+++ tests/functional/module_utils/ims_test_catalog_utils.py
@@ -1,15 +1,92 @@
 class CatalogInputParameters():
   PSBLIB = ["IMSTESTL.IMS1.PSBLIB"]
   DBDLIB = ["IMSTESTL.IMS1.DBDLIB"]
-  ACBLIB = ["IMSTESTL.IMS1.ACBLIB1"]
-  STEPLIB = ["IMSTESTL.IMS1. SDFSRESL"]
+  ACBLIB = ["IMSTESTL.IMS1.ACBLIB"]
+  STEPLIB = ["IMSTESTL.IMS1.SDFSRESL"]
   PROCLIB = 'IMSTESTL.IMS1.PROCLIB'
-  RESLIB = ["IMSTESTL.IMS1. SDFSRESL"]
+  RESLIB = ["IMSTESTL.IMS1.SDFSRESL"]
   BUFFERPOOL = "IMSTESTL.IMS1.PROCLIB(DFSVSMHP)"
   LOADMODE = "LOAD"
   UPDATEMODE = "UPDATE"
   PRIMARYLOG = {
-    "dataset_name": "IMSTESTL.IMS1.LOG",
+    "dataset_name": "IMSTESTL.IMS1.LOG1",
     "disposition": "NEW",
-    "normal_disposition": "DELETE"
+    "normal_disposition": "DELETE",
+    "record_format": "FB",
+    "record_length": "4092",
+    "block_size": "4096",
+    "primary": "100",
+    "primary_unit": "CYL",
+    "secondary": "75",
+    "secondary_unit": "CYL",
+    "type": "SEQ"
   }
+  PURGEMODE = "PURGE"
+  DELETES=[
+    { 
+      "resource": "DBD",
+      "member_name": "DB*",
+      "time_stamp": '*'
+    },
+    { 
+      "resource": "DBD",
+      "member_name": "AUTO*",
+      "time_stamp": '*'
+    },
+    { 
+      "resource": "DBD",
+      "member_name": "DF*",
+      "time_stamp": '*'
+    },
+    { 
+      "resource": "DBD",
+      "member_name": "DI*",
+      "time_stamp": '*'
+    },
+    { 
+      "resource": "DBD",
+      "member_name": "EMP*",
+      "time_stamp": '*'
+    },
+    { 
+      "resource": "DBD",
+      "member_name": "IP*",
+      "time_stamp": '*'
+    },
+    { 
+      "resource": "DBD",
+      "member_name": "IV*",
+      "time_stamp": '*'
+    },
+    { 
+      "resource": "DBD",
+      "member_name": "SI*",
+      "time_stamp": '*'
+    },
+    { 
+      "resource": "PSB",
+      "member_name": "AUT*",
+      "time_stamp": '*'
+    },
+    { 
+      "resource": "PSB",
+      "member_name": "DBF*",
+      "time_stamp": '*'
+    },
+     { 
+      "resource": "PSB",
+      "member_name": "DFH*",
+      "time_stamp": '*'
+    },
+     { 
+      "resource": "PSB",
+      "member_name": "DFSI*",
+      "time_stamp": '*'
+    },
+     { 
+      "resource": "PSB",
+      "member_name": "IP*",
+      "time_stamp": '*'
+    }
+    
+  ]
--- tests/functional/modules/ims_catalog/test_catalog.py
+++ tests/functional/modules/ims_catalog/test_catalog.py
@@ -6,11 +6,13 @@ from pprint import pprint
 from ibm_zos_ims.tests.functional.module_utils.ims_test_catalog_utils import CatalogInputParameters as cp # pylint: disable=import-error
 
 def load_catalog(hosts, validation_msg, mode, psb_lib, dbd_lib, steplib, reslib, proclib, primary_log_dataset,
-          buffer_pool_param_dataset, acb_lib, control_statements=None, bootstrap_dataset=None, 
+          buffer_pool_param_dataset, acb_lib, irlm_enabled=None, irlm_id=None, control_statements=None, bootstrap_dataset=None, 
           directory_dataset=None, temp_acb_dataset=None, directory_staging_dataset=None, 
-          secondary_log_dataset=None, sysabend=None):
+          secondary_log_dataset=None, sysabend=None, check_timestamp=None):
 
     response = hosts.all.ims_catalog_populate(
+        irlm_id=irlm_id,
+        irlm_enabled=irlm_enabled,
         psb_lib=psb_lib,
         dbd_lib=dbd_lib,
         acb_lib=acb_lib,
@@ -18,11 +20,12 @@ def load_catalog(hosts, validation_msg, mode, psb_lib, dbd_lib, steplib, reslib,
         reslib=reslib,
         proclib=proclib,
         sysabend=sysabend,
+        check_timestamp=check_timestamp,
         buffer_pool_param_dataset=buffer_pool_param_dataset,
         mode=mode,
         control_statements=control_statements,
         bootstrap_dataset=bootstrap_dataset,
-        directory_dataset=directory_dataset,
+        directory_datasets=directory_dataset,
         temp_acb_dataset=temp_acb_dataset,
         directory_staging_dataset=directory_staging_dataset,
         primary_log_dataset=primary_log_dataset,
@@ -32,32 +35,36 @@ def load_catalog(hosts, validation_msg, mode, psb_lib, dbd_lib, steplib, reslib,
         pprint(result)
         print("Changed:", result['changed'])
         assert result['changed'] == False
-        assert result['rc'] != 0
+        assert result['rc'] == 0
         assert validation_msg in result['content']
 
-def purge_catalog(hosts, psb_lib, dbd_lib, steplib, reslib, proclib, sysabend,
-          buffer_pool_param_dataset, acb_lib, sysut1, update_retention_criteria,
-          delete, managed_acbs, primary_log_dataset, validation_msg, mode='PURGE'):
+def purge_catalog(hosts, validation_msg, primary_log_dataset, psb_lib, dbd_lib, steplib, reslib, proclib,
+          buffer_pool_param_dataset, irlm_enabled=None, irlm_id=None, sysut1=None, update_retention_criteria=None,
+          delete=None, managed_acbs=None, delete_dbd_by_version=None, resource_chkp_freq=None, mode='PURGE'):
     
-    response = hosts.all.ims_catalog_populate(
+    response = hosts.all.ims_catalog_purge(
+        irlm_id=irlm_id,
+        irlm_enabled=irlm_enabled,
         psb_lib=psb_lib,
         dbd_lib=dbd_lib,
-        acb_lib=acb_lib,
         steplib=steplib,
+        resource_chkp_freq=resource_chkp_freq,
         reslib=reslib,
         proclib=proclib,
-        sysabend=sysabend,
+        update_retention_criteria=update_retention_criteria,
+        delete_dbd_by_version=delete_dbd_by_version,
         buffer_pool_param_dataset=buffer_pool_param_dataset,
         mode=mode,
         primary_log_dataset=primary_log_dataset,
         delete=delete,
-        managed_acbs=managed_acbs
+        managed_acbs=managed_acbs,
+        sysut1=sysut1
         )
     for result in response.contacted.values():
         pprint(result)
         print("Changed:", result['changed'])
         assert result['changed'] == False
-        assert result['rc'] != 0
+        assert result['rc'] == 0
         assert validation_msg in result['content']
 
 
@@ -68,11 +75,24 @@ def test_catalog_load_simple(ansible_zos_module):
                 dbd_lib=cp.DBDLIB, 
                 acb_lib=cp.ACBLIB, 
                 steplib=cp.STEPLIB, 
-                 reslib=cp.RESLIB, 
-                 proclib=cp.PROCLIB, 
-                 primary_log_dataset=cp.PRIMARYLOG, 
-                 buffer_pool_param_dataset=cp.BUFFERPOOL, 
-                 mode=cp.LOADMODE,
-                 validation_msg="DFS4434I")
+                reslib=cp.RESLIB, 
+                proclib=cp.PROCLIB, 
+                primary_log_dataset=cp.PRIMARYLOG, 
+                buffer_pool_param_dataset=cp.BUFFERPOOL, 
+                mode=cp.LOADMODE,
+                validation_msg="DFS4434I")
+
+    purge_catalog(hosts, 
+                psb_lib=cp.PSBLIB, 
+                dbd_lib=cp.DBDLIB, 
+                steplib=cp.STEPLIB, 
+                reslib=cp.RESLIB, 
+                proclib=cp.PROCLIB, 
+                primary_log_dataset=cp.PRIMARYLOG, 
+                buffer_pool_param_dataset=cp.BUFFERPOOL, 
+                mode=cp.PURGEMODE,
+                validation_msg="DFS4518I",
+                delete=cp.DELETES)
+
     
 
