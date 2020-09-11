9421ab1404aa5493070b7c5e2c27ce00c8757034
Dipti <ddgandhi@us.ibm.com>
2020-09-11 16:43:39 -0700

    fix sanity errors for acbgen

--- tests/functional/modules/ims_acb_gen/test_ims_acb_gen_invalid_input.py
+++ tests/functional/modules/ims_acb_gen/test_ims_acb_gen_invalid_input.py
@@ -94,8 +94,8 @@ Work flow for Combination functional tests goes as follows:
 
 
 def validate_acbgen(hosts, psb_name=None, dbd_name=None, psb_lib=None,
-    dbd_lib=None, acb_lib=None, steplib=None, reslib=None,
-    compression=None, build_psb=None, command_input=None):
+                    dbd_lib=None, acb_lib=None, steplib=None, reslib=None,
+                    compression=None, build_psb=None, command_input=None):
     arguments = {}
     if psb_name:
         arguments["psb_name"] = psb_name
@@ -133,144 +133,144 @@ def validate_acbgen(hosts, psb_name=None, dbd_name=None, psb_lib=None,
 # 1. PSB name doesn't exists as string, BUILD PSB=PSB_NAME
 def test_acb_gen_build_invalid_psbName_str(ansible_zos_module):
     hosts = ansible_zos_module
-    validate_acbgen(hosts, command_input=COMMAND_INPUT_BUILD,psb_name=INVALID_PSB, psb_lib=PSBLIB, dbd_lib=DBDLIB, 
+    validate_acbgen(hosts, command_input=COMMAND_INPUT_BUILD, psb_name=INVALID_PSB, psb_lib=PSBLIB, dbd_lib=DBDLIB,
                     acb_lib=ACBLIB, steplib=STEPLIB, reslib=RESLIB, build_psb=True)
 
 
 # 2. Invalid PSBs as list, BUILD PSB=PSB_NAME
 def test_acb_gen_build_invalid_psbNames(ansible_zos_module):
     hosts = ansible_zos_module
-    validate_acbgen(hosts, command_input=COMMAND_INPUT_BUILD, psb_name=INVALID_PSBS, psb_lib=PSBLIB, dbd_lib=DBDLIB, 
+    validate_acbgen(hosts, command_input=COMMAND_INPUT_BUILD, psb_name=INVALID_PSBS, psb_lib=PSBLIB, dbd_lib=DBDLIB,
                     acb_lib=ACBLIB, steplib=STEPLIB, reslib=RESLIB, build_psb=True)
 
 
 # 3. Invalid DBD, BUILD DBD=DBD_NAME
 def test_acb_gen_build_invalid_dbdName(ansible_zos_module):
     hosts = ansible_zos_module
-    validate_acbgen(hosts, command_input=COMMAND_INPUT_BUILD, dbd_name=INVALID_DBD, psb_lib=PSBLIB, dbd_lib=DBDLIB, 
+    validate_acbgen(hosts, command_input=COMMAND_INPUT_BUILD, dbd_name=INVALID_DBD, psb_lib=PSBLIB, dbd_lib=DBDLIB,
                     acb_lib=ACBLIB, steplib=STEPLIB, reslib=RESLIB, build_psb=True)
 
 
 # 4. Invalid DBDs, BUILD DBD=DBD_NAME
 def test_acb_gen_build_invalid_dbdNames(ansible_zos_module):
     hosts = ansible_zos_module
-    validate_acbgen(hosts, command_input=COMMAND_INPUT_BUILD, dbd_name=INVALID_DBDS, psb_lib=PSBLIB, dbd_lib=DBDLIB, 
+    validate_acbgen(hosts, command_input=COMMAND_INPUT_BUILD, dbd_name=INVALID_DBDS, psb_lib=PSBLIB, dbd_lib=DBDLIB,
                     acb_lib=ACBLIB, steplib=STEPLIB, reslib=RESLIB, build_psb=True)
 
 
 # 5. Invalid PSB and invalid DBD, BUILD PSB=PSB_NAME and BUILD DBD=DBD_NAME,BLDPSB=NO
 def test_acb_gen_build_invalid_psbName_list(ansible_zos_module):
     hosts = ansible_zos_module
-    validate_acbgen(hosts, command_input=COMMAND_INPUT_BUILD, psb_name=INVALID_PSB, psb_lib=PSBLIB, dbd_lib=DBDLIB, 
+    validate_acbgen(hosts, command_input=COMMAND_INPUT_BUILD, psb_name=INVALID_PSB, psb_lib=PSBLIB, dbd_lib=DBDLIB,
                     acb_lib=ACBLIB, steplib=STEPLIB, reslib=RESLIB, build_psb=True)
 
 
 def test_acb_gen_build_invalid_psbName_invalid_dbdName(ansible_zos_module):
     hosts = ansible_zos_module
-    validate_acbgen(hosts, command_input=COMMAND_INPUT_BUILD, psb_name=INVALID_PSB, dbd_name=INVALID_DBD, psb_lib=PSBLIB, 
+    validate_acbgen(hosts, command_input=COMMAND_INPUT_BUILD, psb_name=INVALID_PSB, dbd_name=INVALID_DBD, psb_lib=PSBLIB,
                     dbd_lib=DBDLIB, acb_lib=ACBLIB, steplib=STEPLIB, reslib=RESLIB, build_psb=False)
 
 
 # 6. Valid PSB and invalid DBD, BUILD PSB=PSB_NAME and BUILD DBD=DBD_NAME,BLDPSB=YES
 def test_acb_gen_build_valid_psbName_invalid_dbdName(ansible_zos_module):
     hosts = ansible_zos_module
-    validate_acbgen(hosts, command_input=COMMAND_INPUT_BUILD, psb_name=PSB_NAME, dbd_name=INVALID_DBD, psb_lib=PSBLIB, 
+    validate_acbgen(hosts, command_input=COMMAND_INPUT_BUILD, psb_name=PSB_NAME, dbd_name=INVALID_DBD, psb_lib=PSBLIB,
                     dbd_lib=DBDLIB, acb_lib=ACBLIB, steplib=STEPLIB, reslib=RESLIB, build_psb=True)
 
 
 # 7. Invalid PSB and valid DBD, BUILD PSB=PSB_NAME and BUILD DBD=DBD_NAME,BLDPSB=NO
 def test_acb_gen_build_invalid_psbName_valid_dbdName(ansible_zos_module):
     hosts = ansible_zos_module
-    validate_acbgen(hosts, command_input=COMMAND_INPUT_BUILD, psb_name=INVALID_PSB, dbd_name=DBD_NAME, psb_lib=PSBLIB, 
+    validate_acbgen(hosts, command_input=COMMAND_INPUT_BUILD, psb_name=INVALID_PSB, dbd_name=DBD_NAME, psb_lib=PSBLIB,
                     dbd_lib=DBDLIB, acb_lib=ACBLIB, steplib=STEPLIB, reslib=RESLIB, build_psb=False)
 
 
 # 8. Invalid PSB, DELETE PSB=PSB_NAME
 def test_acb_gen_delete_invalid_psbName(ansible_zos_module):
     hosts = ansible_zos_module
-    validate_acbgen(hosts, command_input=COMMAND_INPUT_DELETE, psb_name=INVALID_PSB, psb_lib=PSBLIB, dbd_lib=DBDLIB, 
+    validate_acbgen(hosts, command_input=COMMAND_INPUT_DELETE, psb_name=INVALID_PSB, psb_lib=PSBLIB, dbd_lib=DBDLIB,
                     acb_lib=ACBLIB, steplib=STEPLIB, reslib=RESLIB)
 
 
 # 9. Invalid DBD, DELETE DBD=DBD_NAME
 def test_acb_gen_delete_invalid_dbdName(ansible_zos_module):
     hosts = ansible_zos_module
-    validate_acbgen(hosts, command_input=COMMAND_INPUT_DELETE, dbd_name=INVALID_DBD, psb_lib=PSBLIB, dbd_lib=DBDLIB, 
+    validate_acbgen(hosts, command_input=COMMAND_INPUT_DELETE, dbd_name=INVALID_DBD, psb_lib=PSBLIB, dbd_lib=DBDLIB,
                     acb_lib=ACBLIB, steplib=STEPLIB, reslib=RESLIB)
 
 
 # 10. Invalid PSB and invalid DBD, DELETE PSB=PSB_NAME and DELETE DBD=DBD_NAME
 def test_acb_gen_delete_invalid_psbName_invalid_dbdName(ansible_zos_module):
     hosts = ansible_zos_module
-    validate_acbgen(hosts, command_input=COMMAND_INPUT_DELETE, psb_name=INVALID_PSB, dbd_name=INVALID_DBD, psb_lib=PSBLIB, 
+    validate_acbgen(hosts, command_input=COMMAND_INPUT_DELETE, psb_name=INVALID_PSB, dbd_name=INVALID_DBD, psb_lib=PSBLIB,
                     dbd_lib=DBDLIB, acb_lib=ACBLIB, steplib=STEPLIB, reslib=RESLIB)
 
 
 # 11. Valid PSB and invalid DBD, DELETE PSB=PSB_NAME and DELETE DBD=DBD_NAME
 def test_acb_gen_delete_valid_psbName_invalid_dbdName(ansible_zos_module):
     hosts = ansible_zos_module
-    validate_acbgen(hosts, command_input=COMMAND_INPUT_DELETE, psb_name=PSB_NAME, dbd_name=INVALID_DBD, psb_lib=PSBLIB, 
+    validate_acbgen(hosts, command_input=COMMAND_INPUT_DELETE, psb_name=PSB_NAME, dbd_name=INVALID_DBD, psb_lib=PSBLIB,
                     dbd_lib=DBDLIB, acb_lib=ACBLIB, steplib=STEPLIB, reslib=RESLIB)
 
 
 # 12. Invalid PSB and valid DBD, DELETE PSB=PSB_NAME and DELETE DBD=DBD_NAME
 def test_acb_gen_delete_invalid_psbName_valid_dbdName(ansible_zos_module):
     hosts = ansible_zos_module
-    validate_acbgen(hosts, command_input=COMMAND_INPUT_DELETE, psb_name=INVALID_PSB, dbd_name=DBD_NAME, psb_lib=PSBLIB, 
+    validate_acbgen(hosts, command_input=COMMAND_INPUT_DELETE, psb_name=INVALID_PSB, dbd_name=DBD_NAME, psb_lib=PSBLIB,
                     dbd_lib=DBDLIB, acb_lib=ACBLIB, steplib=STEPLIB, reslib=RESLIB)
 
 
 # 13. Invalid PSBLIB - LIB not exists
 def test_acb_gen_delete_invalid_psblib(ansible_zos_module):
     hosts = ansible_zos_module
-    validate_acbgen(hosts, command_input=COMMAND_INPUT_DELETE, psb_name=PSB_NAME, dbd_name=DBD_NAME, psb_lib=INVALID_PSBLIB, 
+    validate_acbgen(hosts, command_input=COMMAND_INPUT_DELETE, psb_name=PSB_NAME, dbd_name=DBD_NAME, psb_lib=INVALID_PSBLIB,
                     dbd_lib=DBDLIB, acb_lib=ACBLIB, steplib=STEPLIB, reslib=RESLIB)
 
 
 # 14. Invalid DBDLIB - LIB not exists
 def test_acb_gen_delete_invalid_dbdlib(ansible_zos_module):
     hosts = ansible_zos_module
-    validate_acbgen(hosts, command_input=COMMAND_INPUT_DELETE, psb_name=PSB_NAME, dbd_name=DBD_NAME, psb_lib=PSBLIB, 
+    validate_acbgen(hosts, command_input=COMMAND_INPUT_DELETE, psb_name=PSB_NAME, dbd_name=DBD_NAME, psb_lib=PSBLIB,
                     dbd_lib=INVALID_DBDLIB, acb_lib=ACBLIB, steplib=STEPLIB, reslib=RESLIB)
 
 
 # 15. Invalid STEPLIB - LIB authorized and exists
 def test_acb_gen_delete_invalid_steplib(ansible_zos_module):
     hosts = ansible_zos_module
-    validate_acbgen(hosts, command_input=COMMAND_INPUT_DELETE, psb_name=PSB_NAME, dbd_name=DBD_NAME, psb_lib=PSBLIB, 
+    validate_acbgen(hosts, command_input=COMMAND_INPUT_DELETE, psb_name=PSB_NAME, dbd_name=DBD_NAME, psb_lib=PSBLIB,
                     dbd_lib=DBDLIB, acb_lib=ACBLIB, steplib=INVALID_STEPLIB, reslib=RESLIB)
 
 
 # 16. Invalid RESLIB - LIB authorized and exists
 def test_acb_gen_delete_invalid_reslib(ansible_zos_module):
     hosts = ansible_zos_module
-    validate_acbgen(hosts, command_input=COMMAND_INPUT_DELETE, psb_name=PSB_NAME, dbd_name=DBD_NAME, psb_lib=PSBLIB, 
+    validate_acbgen(hosts, command_input=COMMAND_INPUT_DELETE, psb_name=PSB_NAME, dbd_name=DBD_NAME, psb_lib=PSBLIB,
                     dbd_lib=DBDLIB, acb_lib=ACBLIB, steplib=STEPLIB, reslib=INVALID_RESLIB)
 
 
 # 17. Invalid COMP
 def test_acb_gen_build_invalid_comp(ansible_zos_module):
     hosts = ansible_zos_module
-    validate_acbgen(hosts, command_input=COMMAND_INPUT_BUILD, psb_name=PSB_NAME, psb_lib=PSBLIB, dbd_lib=DBDLIB, 
+    validate_acbgen(hosts, command_input=COMMAND_INPUT_BUILD, psb_name=PSB_NAME, psb_lib=PSBLIB, dbd_lib=DBDLIB,
                     acb_lib=ACBLIB, steplib=STEPLIB, reslib=RESLIB, compression=INVALID_COMP, build_psb=True)
 
 
 # 18. PSBLIB with no psbs and DBDLIB populated, BUILD PSB=ALL
 def test_acb_gen_delete_no_psbs(ansible_zos_module):
     hosts = ansible_zos_module
-    validate_acbgen(hosts, command_input=COMMAND_INPUT_DELETE, psb_name=PSB_NAME, dbd_name=DBD_NAME, psb_lib=EMPTY_PSBLIB, 
+    validate_acbgen(hosts, command_input=COMMAND_INPUT_DELETE, psb_name=PSB_NAME, dbd_name=DBD_NAME, psb_lib=EMPTY_PSBLIB,
                     dbd_lib=DBDLIB, acb_lib=ACBLIB, steplib=STEPLIB, reslib=INVALID_RESLIB)
 
 
 # 19. DBDLIB with no dbds and PSBLIB populated, BUILD PSB=ALL
 def test_acb_gen_delete_no_dbds(ansible_zos_module):
     hosts = ansible_zos_module
-    validate_acbgen(hosts, command_input=COMMAND_INPUT_DELETE, psb_name=PSB_NAME, dbd_name=DBD_NAME, psb_lib=PSBLIB, 
+    validate_acbgen(hosts, command_input=COMMAND_INPUT_DELETE, psb_name=PSB_NAME, dbd_name=DBD_NAME, psb_lib=PSBLIB,
                     dbd_lib=EMPTY_DBDLIB, acb_lib=ACBLIB, steplib=STEPLIB, reslib=INVALID_RESLIB)
 
 
 # 20. Invalid ACBLIB
 def test_acb_gen_build_invalid_acblib(ansible_zos_module):
     hosts = ansible_zos_module
-    validate_acbgen(hosts, command_input=COMMAND_INPUT_BUILD, psb_name=PSB_NAME, psb_lib=PSBLIB, dbd_lib=DBDLIB, 
+    validate_acbgen(hosts, command_input=COMMAND_INPUT_BUILD, psb_name=PSB_NAME, psb_lib=PSBLIB, dbd_lib=DBDLIB,
                     acb_lib=INVALID_ACBLIB, steplib=STEPLIB, reslib=RESLIB, compression=COMP, build_psb=False)
