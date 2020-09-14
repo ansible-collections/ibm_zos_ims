1c1f922617c2bf2e51079642ceed1107a7e9bc78
Jerry Li <lij@us.ibm.com>
2020-09-14 12:33:57 -0400

    revert config changes to test folders

--- playbook.sh
+++ playbook.sh
@@ -2,10 +2,10 @@
 ansible-galaxy collection build ./ --force
 ansible-galaxy collection install ibm-*.tar.gz -c --force-with-deps
 
-# plugins_dir=$(pwd)/plugins
-# export ANSIBLE_LIBRARY=${plugins_dir}/modules/
-# export ANSIBLE_MODULE_UTILS=${plugins_dir}/module_utils
-# export ANSIBLE_ACTION_PLUGINS=${plugins_dir}/action
-# export ANSIBLE_CONNECTION_PLUGINS=${plugins_dir}/connection
-# export ANSIBLE_CONFIG=$(pwd)/playbooks/ansible.cfg
-# ansible-playbook -i $1 $2 -vvv
+plugins_dir=$(pwd)/plugins
+export ANSIBLE_LIBRARY=${plugins_dir}/modules/
+export ANSIBLE_MODULE_UTILS=${plugins_dir}/module_utils
+export ANSIBLE_ACTION_PLUGINS=${plugins_dir}/action
+export ANSIBLE_CONNECTION_PLUGINS=${plugins_dir}/connection
+export ANSIBLE_CONFIG=$(pwd)/playbooks/ansible.cfg
+ansible-playbook -i $1 $2 -vvv
