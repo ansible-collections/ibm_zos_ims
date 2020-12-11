9d76f36e895b64b6aa5ffb42ecfdc1d42efbf3dc
Ketan Kelkar <ktnklkr@gmail.com>
2020-12-11 11:56:25 -0800

    add dbdgen and psbgen prereq tests to acbgen test suite to remove dependency on dbdgen and psbgen test suite

--- tests/functional/modules/ims_acb_gen/test_ims_acb_gen_valid_input.py
+++ tests/functional/modules/ims_acb_gen/test_ims_acb_gen_valid_input.py
@@ -3,7 +3,8 @@ from __future__ import (absolute_import, division, print_function)
 from pprint import pprint
 import pytest
 from ibm_zos_ims.tests.functional.module_utils.ims_test_gen_utils import ACBInputParameters as ip
-
+from ibm_zos_ims.tests.functional.module_utils.ims_test_gen_utils import DBDInputParameters as dbd
+from ibm_zos_ims.tests.functional.module_utils.ims_test_gen_utils import PSBInputParameters as psb
 
 __metaclass__ = type
 
@@ -23,6 +24,11 @@ DBD_NAMES_LIST = ip.DBD_NAMES_LIST
 COMP_PRE = ip.COMP_PRE
 COMP_POST = ip.COMP_POST
 COMP = ip.COMP
+
+# vars for prereqs
+DBD_SRC = dbd.SOURCE
+PSB_SRC = psb.SOURCE
+SYSLIB = dbd.SYSLIB
 
 
 """
@@ -83,6 +89,38 @@ def validate_acbgen(hosts, psb_name=None, dbd_name=None, psb_lib=None,
         print("Return code:", result.get('rc'))
         assert result.get('changed')
         assert result.get('rc') <= 4
+
+def test_dbd_gen_dataset_prereq(ansible_zos_module):
+    hosts = ansible_zos_module
+    # validate_single_src(hosts, DESTINATION, SYSLIB, src=SOURCE, location='DATA_SET', member_list=['DEDBJN21'], replace=True)
+    response = hosts.all.ims_dbd_gen(
+        dest=DBDLIB[0],
+        sys_lib=SYSLIB, src=DBD_SRC, location="DATA_SET",
+        replace=True,
+        member_list=[
+            "DH41SK01", "DBFSAMD1", "DH41SK01", "DBFSAMD2", "DBFSAMD3", "HOSPVARD", "DSVNTZ30", "DX41SK01",
+            "DX41SK03", "DX41SK05", "DX41SK06", "DX41SK07", "DX41SK08", "DX41SK09", "DX41SK02", "DX41SK04", "WAREDB",
+            "ORDDB", "DISTDB", "ARTDB", "CUSTDB", "NORDDB", "ORDRDB", "ORDLDB", "ITEMDB", "ITEMDBP", "STCKDB"])
+    for result in response.contacted.values():
+        print(result)
+        print("Changed:", result['changed'])
+        assert result['changed']
+        assert result['rc'] == 0
+        # Check for success message (if we remove return codes)
+        # assert result['msg'] == GEN_SUCCESS_MSG
+
+def test_psb_gen_dataset_prereq(ansible_zos_module):
+    hosts = ansible_zos_module
+    response = hosts.all.ims_psb_gen(
+        dest=PSBLIB[0], sys_lib=SYSLIB, src=PSB_SRC, location="DATA_SET",
+        replace=True, member_list=["PSBGENL"])
+    for result in response.contacted.values():
+        pprint(result)
+        print("Changed:", result['changed'])
+        assert result['changed']
+        assert result['rc'] == 0
+        # Check for success message (if we remove return codes)
+        # assert result['msg'] == GEN_SUCCESS_MSG
 
 
 # 1. BUILD PSB=ALL as string
