09f72a6f29ac3f4834e686146ad537e4cefe4870
BRYANT PANYARACHUN <bpanyarachun@gmail.com>
2020-12-18 17:11:11 -0800

    continuation line under-indented for hanging indent

--- plugins/module_utils/dbrc.py
+++ plugins/module_utils/dbrc.py
@@ -26,10 +26,14 @@ class dbrc():
 
     def __init__(self, commands, steplib, dynamic_allocation_dataset=None, dbd_lib=None,
 <<<<<<< HEAD
+<<<<<<< HEAD
                  genjcl_output_dataset=None, genjcl_input_dataset=None, recon1=None, recon2=None, recon3=None):
 =======
     genjcl_output_dataset=None, genjcl_input_dataset=None, recon1=None, recon2=None, recon3=None):
 >>>>>>> white space and expected blank lines.
+=======
+        genjcl_output_dataset=None, genjcl_input_dataset=None, recon1=None, recon2=None, recon3=None):
+>>>>>>> continuation line under-indented for hanging indent
         """IMSDBRC constructor used to run DBRC commands using zos_raw.
 
         Args:
