ffd7837f0e649105bd6dd7fbd12cb1579ce58ead
BRYANT PANYARACHUN <bpanyarachun@gmail.com>
2020-12-23 15:18:07 -0800

    test_catalog continuation line under-indented for visual indent.

--- tests/functional/modules/ims_catalog/test_catalog.py
+++ tests/functional/modules/ims_catalog/test_catalog.py
@@ -14,31 +14,31 @@ __metaclass__ = type
 def test_catalog_load_simple(ansible_zos_module):
     hosts = ansible_zos_module
     load_catalog(hosts,
-                psb_lib=cp.PSBLIB,
-                dbd_lib=cp.DBDLIB,
-                acb_lib=cp.ACBLIB,
-                steplib=cp.STEPLIB,
-                reslib=cp.RESLIB,
-                proclib=cp.PROCLIB,
-                modstat=cp.MODSTAT,
-                primary_log_dataset=cp.PRIMARYLOG,
-                buffer_pool_param_dataset=cp.BUFFERPOOL,
-                mode=cp.LOADMODE,
-                validation_msg="")  # Don't check for a message since the message is different depending on the configuration
+                 psb_lib=cp.PSBLIB,
+                 dbd_lib=cp.DBDLIB,
+                 acb_lib=cp.ACBLIB,
+                 steplib=cp.STEPLIB,
+                 reslib=cp.RESLIB,
+                 proclib=cp.PROCLIB,
+                 modstat=cp.MODSTAT,
+                 primary_log_dataset=cp.PRIMARYLOG,
+                 buffer_pool_param_dataset=cp.BUFFERPOOL,
+                 mode=cp.LOADMODE,
+                 validation_msg="")  # Don't check for a message since the message is different depending on the configuration
 
     purge_catalog(hosts,
-                psb_lib=cp.PSBLIB,
-                dbd_lib=cp.DBDLIB,
-                steplib=cp.STEPLIB,
-                reslib=cp.RESLIB,
-                proclib=cp.PROCLIB,
-                primary_log_dataset=cp.PRIMARYLOG,
-                buffer_pool_param_dataset=cp.BUFFERPOOL,
-                mode=cp.PURGEMODE,
-                validation_msg="DFS4518I",
-                # validation_msg="",
-                sysut1=cp.SYSUT1,
-                delete=cp.DELETES)
+                  psb_lib=cp.PSBLIB,
+                  dbd_lib=cp.DBDLIB,
+                  steplib=cp.STEPLIB,
+                  reslib=cp.RESLIB,
+                  proclib=cp.PROCLIB,
+                  primary_log_dataset=cp.PRIMARYLOG,
+                  buffer_pool_param_dataset=cp.BUFFERPOOL,
+                  mode=cp.PURGEMODE,
+                  validation_msg="DFS4518I",
+                  # validation_msg="",
+                  sysut1=cp.SYSUT1,
+                  delete=cp.DELETES)
 
 
 
