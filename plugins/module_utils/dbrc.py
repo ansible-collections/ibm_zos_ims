57a8814d3a1d605f00a4adc5ccc6a730ade4d276
BRYANT PANYARACHUN <bpanyarachun@gmail.com>
2020-12-22 16:22:53 -0800

    continuation line with same indent as next logical line

--- plugins/module_utils/dbrc.py
+++ plugins/module_utils/dbrc.py
@@ -25,7 +25,7 @@ class dbrc():
     }
 
     def __init__(self, commands, steplib, dynamic_allocation_dataset=None, dbd_lib=None,
-        genjcl_output_dataset=None, genjcl_input_dataset=None, recon1=None, recon2=None, recon3=None):
+                genjcl_output_dataset=None, genjcl_input_dataset=None, recon1=None, recon2=None, recon3=None):
         """IMSDBRC constructor used to run DBRC commands using zos_raw.
 
         Args:
