e71203d7b2f7f375838c93f551cb7d6a536abdab
BRYANT PANYARACHUN <bpanyarachun@gmail.com>
2020-12-22 16:34:19 -0800

    the backslash is redundant between brackets

--- plugins/module_utils/dbrc.py
+++ plugins/module_utils/dbrc.py
@@ -163,7 +163,7 @@ class dbrc():
                 except:
                     key = unformatted_key.strip()
             if len(elements) != 2 and \
-                (elements[i + 1][0:2] == double_space or \
+                (elements[i + 1][0:2] == double_space or
                 (len(value_list) == 1 and value_index > 0 and value_index < len(elements) - 1)):
 
                 fields[key] = None
