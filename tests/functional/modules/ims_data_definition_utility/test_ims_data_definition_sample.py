afce7aa10acf68ae078ca548507f896e7cedad13
trevinop36 <126907790+trevinop36@users.noreply.github.com>
2023-05-18 21:29:06 -0600

    Update test_ims_data_definition_sample.py

--- tests/functional/modules/ims_data_definition_utility/test_ims_data_definition_sample.py
+++ tests/functional/modules/ims_data_definition_utility/test_ims_data_definition_sample.py
@@ -4,42 +4,36 @@ from __future__ import (absolute_import, division, print_function)
 
 from pprint import pprint
 from ibm_zos_ims.tests.functional.module_utils.ims_test_data_definition_utils import ZDDLInputParameters as ip
-from ibm_zos_ims.tests.functional.module_utils.ims_test_gen_utils import DBDInputParameters as dbd
-from ibm_zos_ims.tests.functional.module_utils.ims_test_gen_utils import PSBInputParameters as psb
 
 import pytest
 __metaclass__ = type
 
-
-ONLINE_BATCH_TRUE = ip.ONLINE_BATCH_TRUE # In the documentation it appears as online, but in the examples it's written as online_batch
-ONLINE_BATCH_FALSE = ip.ONLINE_BATCH_FALSE # In the documentation it appears as online, but in the examples it's written as online_batch
-IMS_ID = ip.ID
+# ------------- VARIABLES
+ONLINE = ip.ONLINE 
+OFFLINE = ip.OFFLINE
+IMS_ID = ip.IMS_ID
 IRLM_ID = ip.IRLM_ID # I made up the value for this variable
-RESLIB = ip.RESLIB
-PROCLIB = ip.PROCLIB # The content in the test utils file must have a list with possible values for this variable, for now, I'll just put "PROCLIB"
+RESLIB = ip.RESLIB  # The next 4 variables are lists
+PROCLIB = ip.PROCLIB 
 STEPLIB = ip.STEPLIB
-IMS_SQL = ip.IMS_SQL # In the documentation appears as SQL_input, but in the examples it's declared as ims_sql
+SQL_INPUT = ip.SQL_INPUT 
+
+# Control statements
 VERBOSE = ip.VERBOSE
 AUTO_COMMIT = ip.AUTO_COMMIT
 SIMULATE = ip.SIMULATE
 CREATE_PROGRAM_VIEW = ip.CREATE_PROGRAM_VIEW
-
-# vars for prereqs
-COMMAND_INPUT_BUILD = ip.COMMAND_INPUT_BUILD
-ACBLIB = ip.ACBLIB
-PSBLIB = ip.PSBLIB
-DBDLIB = ip.DBDLIB
-DBD_NAME = ip.DBD_NAME
-DBD_NAMES = ip.DBD_NAMES
+# -------------
 
 
-def validate_data_definition(hosts, online_batch=None, ims_id=None,
-                            irlm_id=None, reslib=None, proclib=None,
-                            steplib=None, ims_sql=None, verbose=None,
-                            auto_commit=None, simulate=None, create_program_view=None):
+def validate_data_definition(hosts, online:bool=None, ims_id:str=None,
+                            irlm_id:str=None, reslib:list=None, proclib:list=None,
+                            steplib:list=None, sql_input:list=None, verbose:bool=None,
+                            auto_commit:bool=None, simulate:bool=None, create_program_view:bool=None
+                            ):
     arguments = {}
-    if online_batch:
-        arguments["online_batch"] = online_batch
+    if online:
+        arguments["online"] = online
     if ims_id:
         arguments["ims_id"] = ims_id
     if irlm_id:
@@ -50,8 +44,8 @@ def validate_data_definition(hosts, online_batch=None, ims_id=None,
         arguments["proclib"] = proclib
     if steplib:
         arguments["steplib"] = steplib
-    if ims_sql:
-        arguments["ims_sql"] = ims_sql
+    if sql_input:
+        arguments["sql_input"] = sql_input
     if verbose:
         arguments["verbose"] = verbose
     if auto_commit:
@@ -64,39 +58,22 @@ def validate_data_definition(hosts, online_batch=None, ims_id=None,
     print("Result:", response)
     for result in response.contacted.values():
         pprint(result)
-        #print("Changed:", result.get('changed')) # No change variable
         print("Return code:", result.get('rc'))
-        #assert result.get('changed')
         assert result.get('rc') <= 4
 
 
-def test_managed_acb_prereq(ansible_zos_module):
+def test_ims_data_definition(ansible_zos_module):
     hosts = ansible_zos_module
-    response = hosts.all.ims_acb_gen(
-         # set the instructions for the ACB GEN validation
-        command_input=COMMAND_INPUT_BUILD, 
-        aclib=ACBLIB, 
-        psb_lib=PSBLIB,
-        dbd_lib=DBDLIB, 
-    )
-    for result in response.contacted.values():
-        print(result)
-        print("Changed:", result['changed'])
-        assert result['changed']
-        assert result['rc'] == 0
-
-
-def test_ims_data_definition_online(ansible_zos_module):
-    hosts = ansible_zos_module
-    validate_data_definition(hosts, online_batch=ONLINE_BATCH_TRUE, ims_id=IMS_ID,
+    validate_data_definition(hosts, online=ONLINE, ims_id=IMS_ID,
                             irlm_id=IRLM_ID, reslib=RESLIB, proclib=PROCLIB,
-                            steplib=STEPLIB, ims_sql=IMS_SQL, verbose=VERBOSE,
+                            steplib=STEPLIB, sql_input=SQL_INPUT, verbose=VERBOSE,
                             auto_commit=AUTO_COMMIT, simulate=SIMULATE, create_program_view=CREATE_PROGRAM_VIEW)
 
 
-def test_ims_data_definition_offline(ansible_zos_module):
+
+def test_ims_data_definition_offline(ansible_zos_module): # Delete this ????????????
     hosts = ansible_zos_module
-    validate_data_definition(hosts, online_batch=ONLINE_BATCH_FALSE, ims_id=IMS_ID,
+    validate_data_definition(hosts, online=OFFLINE, ims_id=IMS_ID,
                             irlm_id=IRLM_ID, reslib=RESLIB, proclib=PROCLIB,
-                            steplib=STEPLIB, ims_sql=IMS_SQL, verbose=VERBOSE,
+                            steplib=STEPLIB, sql_input=SQL_INPUT, verbose=VERBOSE,
                             auto_commit=AUTO_COMMIT, simulate=SIMULATE, create_program_view=CREATE_PROGRAM_VIEW)
