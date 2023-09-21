65ea2140cff2bacbb3a0df3c328723c501852bd6
trevinop36 <126907790+trevinop36@users.noreply.github.com>
2023-09-21 16:19:43 -0600

    Test catalog files

---
+++ tests/functional/modules/ims_catalog/test_catalog_valid_input.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+from __future__ import (absolute_import, division, print_function)
+import pytest
+from pprint import pprint
+import sys
+# sys.path.append("/Users/pedrotrevino/Desktop/Coding/stephanie_zos_ibm_ims/ibm_zos_ims")
+
+from ibm_zos_ims.tests.functional.module_utils.ims_test_catalog_utils import CatalogInputParameters as cp  # pylint: disable=import-error
+from ibm_zos_ims.tests.functional.module_utils.ims_test_catalog_utils import load_catalog, purge_catalog  # pylint: disable=import-error
+__metaclass__ = type
+
+
+# 1 - In populate test case, we check that the length of the dfsdf member can't be more than 3 characters
+def test_populate_valid_default(ansible_zos_module):
+    hosts = ansible_zos_module
+    load_catalog(hosts,
+                 psb_lib=cp.PSBLIB,
+                 dbd_lib=cp.DBDLIB,
+                 acb_lib=cp.ACBLIB,
+                 steplib=cp.STEPLIB,
+                 reslib=cp.RESLIB,
+                 proclib=cp.PROCLIB,
+                 modstat=cp.MODSTAT,
+                 primary_log_dataset=cp.PRIMARYLOG,
+                 buffer_pool_param_dataset=cp.BUFFERPOOL,
+                 dfsdf_member="CAT",
+                 mode=cp.LOADMODE,
+                 validation_msg="",
+                 rc=0
+                 ) 
+    
+# 2 - In purge test case, we check that the length of the dfsdf member can't be more than 3 characters
+def test_purge_valid_default(ansible_zos_module):
+    hosts = ansible_zos_module
+    purge_catalog(hosts,
+                  psb_lib=cp.PSBLIB,
+                  dbd_lib=cp.DBDLIB,
+                  steplib=cp.STEPLIB,
+                  reslib=cp.RESLIB,
+                  proclib=cp.PROCLIB,
+                  primary_log_dataset=cp.PRIMARYLOG,
+                  buffer_pool_param_dataset=cp.BUFFERPOOL,
+                  dfsdf_member="CAT",
+                  mode=cp.PURGEMODE,
+                  validation_msg="",
+                  sysut1=cp.SYSUT1,
+                  rc=0,
+                  delete=cp.DELETES)
+
+
