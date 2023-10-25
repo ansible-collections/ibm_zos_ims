03af7874caffd9c7aed6b09f5c66ede9cd36af55
Dipti <ddgandhi@us.ibm.com>
2023-10-25 10:46:08 -0700

    updated run.sh for cicd
    Signed-off-by: Dipti <ddgandhi@us.ibm.com>

--- tests/run.sh
+++ tests/run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-ansible-galaxy collection build ../ --force
-ansible-galaxy collection install ./ibm-ibm_zos_ims*.tar.gz -c -f --force-with-deps
+# ansible-galaxy collection build ../ --force
+# ansible-galaxy collection install ./ibm-ibm_zos_ims*.tar.gz -c -f --force-with-deps
 
-# Installs directory from galaxy
-ansible-galaxy collection install -fc ibm.ibm_zos_core:1.5.0
+# # Installs directory from galaxy
+# ansible-galaxy collection install -fc ibm.ibm_zos_core:1.5.0
 
 current_dir=$(pwd)
 plugins_dir=${current_dir}/../plugins
@@ -13,6 +13,7 @@ export ANSIBLE_LIBRARY=${plugins_dir}/modules/:${core_dir}/modules
 export ANSIBLE_MODULE_UTILS=${plugins_dir}/module_utils${core_dir}/module_utils
 export ANSIBLE_ACTION_PLUGINS=${plugins_dir}/action
 export ANSIBLE_CONNECTION_PLUGINS=${plugins_dir}/connection
+export ANSIBLE_COLLECTIONS_PATH=/home/jenkins/.ansible/collections/ansible_collections
 export ANSIBLE_CONFIG=${current_dir}/ansible.cfg
 
 python3 -m pytest --host-pattern=all --zinventory="${1:-test_config.yml}" "$2" -vvv
